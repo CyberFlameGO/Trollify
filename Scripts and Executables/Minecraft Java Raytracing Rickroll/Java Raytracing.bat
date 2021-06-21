6252e7c134ce778f6b89fa82ffc0e3898dea28a1
Bharath <63896418+techpointdev@users.noreply.github.com>
2021-06-21 18:06:15 +0530

    Add files via upload

---
+++ Scripts and Executables/Minecraft Java Raytracing Rickroll/Java Raytracing.bat
@@ -0,0 +1,7 @@
+@echo off
+title Congrats on getting rickrolled! - TechPoint
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
